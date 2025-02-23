3f2265e6c3c35843abf5687e6469c1d6b4965e0a
Raju kumar <rajuvis778@gmail.com>
2025-02-23 18:57:10 +0530

    changed cors and package.json

--- src/app.js
+++ src/app.js
@@ -6,11 +6,9 @@ const app = express();
 
 // cors
 app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-  })
+  cors({ origin: '*', methods: 'GET,POST,PUT,DELETE,PATCH', credentials: true })
 );
+app.options('*', cors());
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
