0ca136243360985e75f87e87a4dd0840723e1feb
Raju kumar <rajuvis778@gmail.com>
2025-02-23 19:17:35 +0530

    change port

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ dotenv.config({ path: './.env' });
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`⚙️  Server is running on port ${process.env.PORT}`);
     });
     app.on('error', (error) => console.log('ERROR', error));
